72455f5031663b0a8d91cf71db5da693ae6b429a
Ernst Widerberg <ernst@sunet.se>
2021-12-15 10:04:40 +0100

    Quiet docker build output

--- sp-gen.sh
+++ sp-gen.sh
@@ -14,7 +14,7 @@ ID=$URL
 export ID
 
 if [[ ! -f "sp-key.pem" && ! -f "sp-cert.pem" ]]; then
-    docker build -t sp-gen .
+    docker build -q -t sp-gen .
     docker run --rm -d -it --name sp-gen sp-gen
     docker exec sp-gen shib-keygen -h $ID
     docker cp sp-gen:/etc/shibboleth/sp-key.pem .
